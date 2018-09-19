e3aadd52cff9e7942b21217635a529051ded91a8
daniel.teske <daniel.teske@mbition.io>
2018-09-19 13:36:08 +0200

    cleanup

--- heatmap-creation/script.r
+++ heatmap-creation/script.r
@@ -1,19 +1,13 @@
-install.packages("data.table")
 install.packages("RcppCCTZ") #for parseDatetime
-library(data.table)
 library(RcppCCTZ)
 
 setwd("~/projects/stay-safe/heatmap-creation/")
 
-data <- system.time(fread('../data/objectstream.csv', header = T, sep = ',')) 
 data <- read.csv('../data/objectstream.csv', header = T)
-
+data$timestamp2 <- parseDatetime(data$timestamp, "%Y-%m-%dT%H:%M:%SZ");
 head(data)
 
-data_subset <- subset(data, timestamp >= "2018-06-21T17:00:00Z" & timestamp <= "2018-06-21T17:05:00Z")
-
-parseDatetime("2018-06-21T17:00:00Z", "%Y-%m-%d'T'%H:%M:%S'Z'");
-
-data_subset$timestamp2 <- parseDatetime(data_subset$timestamp, "%Y-%m-%d'T'%H:%M:%S'Z'");
-
-data_subset
+filter_from <- parseDatetime("2018-06-21T17:00:00Z", "%Y-%m-%dT%H:%M:%SZ");
+filter_to <- parseDatetime("2018-06-21T17:00:00Z", "%Y-%m-%dT%H:%M:%SZ");
+data_subset <- subset(data, timestamp2 >= filter_from & timestamp2 <= filter_to)
+head(data_subset)
