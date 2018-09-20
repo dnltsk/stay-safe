27ae49e40eea0f224d09bddca066aaeb5f870dee
daniel.teske <daniel.teske@mbition.io>
2018-09-21 00:12:16 +0200

    :whale: dockerized!

--- heatmap-creation/script.r
+++ heatmap-creation/script.r
@@ -62,55 +62,87 @@ data_points <- data.frame(x=data_subset$x, y=data_subset$y)
 P <- as.ppp(data_points, c(min_x,max_x,min_y,max_y), checkdup=F)
 Z <- density(P, bw=.5)
 
-col_brakes <- quantile(Z, probs = (0:4)/4)
+col_breakes
+col_breakes <- as.vector(quantile(Z, probs = (0:4)/4))
 
 #
 # filtering data
 #
-for (hour in 3:23){
-  hour <- str_pad(hour, 2, pad = "0")
-  for (minute in 0:58){
-    
-    if(hour == "03" & (minute == 13 | minute == 14){
-      next
-    }
-    
-    #hour <- 5
-    #minute <- 30
-    from_minute <- str_pad(minute, 2, pad = "0")
-    to_minute <- str_pad(minute+1, 2, pad = "0")
-    from_time<- paste(hour, ":", from_minute, sep="")
-    to_time<- paste(hour, ":", to_minute, sep="")
-    print(paste("from", from_time, "to", to_time))
-    
-    from_timestamp <- paste("2018-06-21T",from_time,":00Z", sep="")
-    to_timestamp <- paste("2018-06-21T",to_time,":00Z", sep="")
-    filter_from <- parseDatetime(from_timestamp, "%Y-%m-%dT%H:%M:%SZ");
-    filter_to <- parseDatetime(to_timestamp, "%Y-%m-%dT%H:%M:%SZ");    
-    
-    data_subset <- subset(data, timestamp2 >= filter_from & timestamp2 <= filter_to)
-    data_points <- data.frame(x=data_subset$x, y=data_subset$y)
-    
-    #
-    # plot
-    #
-    filename <- paste("plots/", from_time,".png", sep = "")
-    png(filename=filename, width = 800, height = 180)
-    
-    P <- as.ppp(data_points, c(min_x,max_x,min_y,max_y), checkdup=F)
-    Z <- density(P, bw=.5)
-    Q <- quadratcount(P, nx= 6, ny=3)
-    
-    #plot(P, pch=20, cols="grey70", main=NULL)
-    #plot(Q, add=TRUE)
-    #Q.d <- intensity(Q)
-    par(mar=c(0,0,1.5,0))
+for (from_hour in 4:23){
+  for (from_minute in 0:59){
+    for (from_second in 0:5){
       
-    plot(intensity(Q, image=TRUE), main = from_timestamp,
-         col = pal_opaque, breaks = col_brakes, legend = c("1", "1", "1", "1"))
-    plot(P, pch=20, cex=0.2, add=T)
+      from_hour <- 0
+      from_minute <- 0
+      from_second <- 5
+      
+      from_second <- from_second * 10
     
-    dev.off()
+      to_hour <- from_hour
+      to_minute <- from_minute
+      to_second <- from_second + 10
+      
+      if(from_second == 50){
+        to_second <- 0
+        to_minute <- from_minute +1
+        if(from_minute == 59){
+          to_minute <- 0
+          to_hour <- from_hour + 1
+        }
+      }
+      
+      from_hour <- str_pad(from_hour, 2, pad = "0")
+      to_hour <- str_pad(to_hour, 2, pad = "0")
+      from_minute <- str_pad(from_minute, 2, pad = "0")
+      to_minute <- str_pad(to_minute, 2, pad = "0")
+      from_second <- str_pad(from_second, 2, pad = "0")
+      to_second <- str_pad(to_second, 2, pad = "0")
+      
+      from_time<- paste(from_hour, ":", from_minute, ":", from_second, sep="")
+      to_time<- paste(to_hour, ":", to_minute, ":", to_second, sep="")
+      
+      print(paste("from", from_time, "to", to_time))
+      
+      from_timestamp <- paste("2018-06-21T",from_time,"Z", sep="")
+      to_timestamp <- paste("2018-06-21T",to_time,"Z", sep="")
+      filter_from <- parseDatetime(from_timestamp, "%Y-%m-%dT%H:%M:%SZ");
+      filter_to <- parseDatetime(to_timestamp, "%Y-%m-%dT%H:%M:%SZ");    
+      
+      data_subset <- subset(data, timestamp2 >= filter_from & timestamp2 <= filter_to)
+      data_points <- data.frame(x=data_subset$x, y=data_subset$y)
+      
+      #
+      # plot
+      #
+      filename <- paste("plots/", from_time,".png", sep = "")
+      png(filename=filename, width = 800, height = 180)
+      
+      P <- as.ppp(data_points, c(min_x,max_x,min_y,max_y), checkdup=F)
+      Z <- density(P, bw=.5)
+      
+      par(mar=c(0,0,1.5,0))
+      if(nrow(data_subset) == 0){
+        plot(P, 
+             #col = pal_opaque,
+             breaks = col_breakes,
+             legend = col_breakes,
+             zlim=c(min(col_breakes), max(col_breakes))
+        )
+      }else{
+        Q <- quadratcount(P, nx= 6, ny=3)  
+        plot(intensity(Q, image=TRUE), main = from_timestamp,
+             col = pal_opaque,
+             #breaks = c(0.001,0.002,0.003,0.004,0.005) #col_breakes
+             breaks = col_breakes,
+             legend = col_breakes,
+             zlim=c(min(col_breakes), max(col_breakes))
+        )
+        plot(P, pch=20, cex=0.3, add=T)
+      }
+      
+      dev.off()
+    
+    }
   }
 }
 
---
+++ server/README.md
@@ -0,0 +1,16 @@
+Stay Save Server
+===
+
+## build
+
+`docker build . -t stay-safe-server`
+
+## run
+
+`docker run -it -p 8000:8000 stay-safe-server`
+
+## stop
+
+`docker ps -all` to get container id
+
+`docker stop [CONTAINER ID]`
